6e3c9cae953f58ebb920f178b4ca3f165991e6bf
SAFFR0NM0NEY <72283367+SAFFR0NM0NEY@users.noreply.github.com>
2025-05-22 08:48:19 +0200

    update

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,3 +1,9 @@
+// Toggle light/dark mode
 document.getElementById("themeToggle").addEventListener("click", () => {
-  document.body.toggleAttribute("data-theme");
+  const currentTheme = document.body.getAttribute("data-theme");
+  if (currentTheme === "dark") {
+    document.body.removeAttribute("data-theme");
+  } else {
+    document.body.setAttribute("data-theme", "dark");
+  }
 });
